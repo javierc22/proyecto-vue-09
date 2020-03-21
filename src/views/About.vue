f6e841d3dddbb8c774b4935c72fd656afb0962c2
javierc22 <javier.ec.22@gmail.com>
2020-03-21 13:10:45 -0300

    Módulos en VUEX

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Título: {{ titulo }}</h1>
+    <h2 v-for="(item, index) in tareasState" :key="index">{{ item }}</h2>
+    <h3>Contador: {{ contador }}</h3>
+    <button @click="aumentarContador(1)">Aumentar contador</button>
   </div>
 </template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(['titulo']),
+    ...mapState('tareas', ['tareasState']),
+    ...mapState('contador', ['contador']),
+  },
+  methods: {
+    ...mapMutations('contador', ['aumentarContador']),
+  },
+}
+</script>
